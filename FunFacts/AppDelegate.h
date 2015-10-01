aefb74da1f7640218bc80e864949da8aa27540eb
Alextnelson <alexander.thomas.nelson@gmail.com>
2015-10-01 12:17:40 -0500

    Initial Commit

---
+++ FunFacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunFacts
+//
+//  Created by Alexander Nelson on 10/1/15.
+//  Copyright © 2015 Jetwolfe Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunFacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunFacts
+//
+//  Created by Alexander Nelson on 10/1/15.
+//  Copyright © 2015 Jetwolfe Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
